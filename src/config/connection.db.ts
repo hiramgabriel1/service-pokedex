fb3e786fca2950223f070a9460b171ff0923d9a7
itsrusty <rusty3031@gmail.com>
2024-05-24 19:04:09 -0700

    feat: change cors error and database added

--- src/config/connection.db.ts
+++ src/config/connection.db.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const MONGO_URI = process.env.URI || '';
 
-export const database= async () => {
+export const database = async () => {
   try {
     await mongoose.connect(MONGO_URI);
     console.log("connected");
--- src/server.ts
+++ src/server.ts
@@ -7,17 +7,19 @@ import routerPokemon from "./routes/pokemon.routes";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 import path from "path";
+import { database } from "./config/connection.db";
 
 dotenv.config();
+database();
 
 const server = express();
 const PORT = process.env.PORT;
 const rateLimitMiddleware = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100,
-  standardHeaders: "draft-7",
-  legacyHeaders: false,
-  message: "limite de peticiones excedido",
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100,
+    standardHeaders: "draft-7",
+    legacyHeaders: false,
+    message: "limite de peticiones excedido",
 });
 
 const swaggerDocumentPath = path.join(__dirname, "config", "swagger.yaml");
@@ -25,11 +27,11 @@ const swaggerDocument = YAML.load(swaggerDocumentPath);
 
 server.use(express.json());
 server.use(
-  cors({
-    origin: ["http://localhost:3000/", "http://localhost:3001/"],
-    methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
-    maxAge: 86400,
-  })
+    cors({
+        origin: ["http://localhost:3000", "http://localhost:3001"],
+        methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
+        maxAge: 86400,
+    })
 );
 
 // todo: middlewares & logger
@@ -41,8 +43,8 @@ server.use(routerPokemon);
 server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const bootstrap = () => {
-  server.listen(PORT || 4000);
-  console.log(`http://localhost:${PORT}`);
+    server.listen(PORT || 4000);
+    console.log(`http://localhost:${PORT}`);
 };
 
 bootstrap();
